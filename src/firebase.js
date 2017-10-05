cd81e496088fb2775222820e85c7b10d739a27ac
michu1234 <moooderator@gmail.com>
2017-10-05 21:54:40 +0200

    start

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import {initializeApp} from 'firebase'
+
+const app = initializeApp({
+    apiKey: "AIzaSyADrHG0aZ1mcFmMFbtcF5Fw-Mlz6IvmfcQ",
+    authDomain: "jobs-board-e7be4.firebaseapp.com",
+    databaseURL: "https://jobs-board-e7be4.firebaseio.com",
+    projectId: "jobs-board-e7be4",
+    storageBucket: "jobs-board-e7be4.appspot.com",
+    messagingSenderId: "738430337246"
+  })
